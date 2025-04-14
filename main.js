4e8bf4698500a0dc2581d27cbb845d24d95316cf
keagantooker <keagantooker@icloud.com>
2025-04-13 23:27:46 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+<script>
+  async function fetchIPInfo() {
+  try {
+  const res = await fetch('https://ipapi.co/json/');
+  const data = await res.json();
+
+  const ip = data.ip;
+  const isp = data.org;
+  const city = data.city;
+  const region = data.region;
+  const country = data.country_name;
+
+  const now = new Date();
+  const time = now.toLocaleTimeString();
+  const date = now.toLocaleDateString();
+
+  document.getElementById("ip-info-bar").innerHTML = `
+        <span class="syntax-comment">// Connected from ${ip} (${isp})</span><br>
+        <span class="syntax-comment">// Location: ${city}, ${region}, ${country}</span><br>
+        <span class="syntax-comment">// Local time: ${time} on ${date}</span>
+      `;
+} catch (err) {
+  document.getElementById("ip-info-bar").innerHTML = `
+        <span class="syntax-comment">// Failed to retrieve IP info</span>
+      `;
+  console.error(err);
+}
+}
+
+  fetchIPInfo();
+</script>
